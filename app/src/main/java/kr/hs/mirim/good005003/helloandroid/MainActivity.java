e5beb4924157daa89228b3cb8d329f369f1082c2
Dakyeong Lee <dlekrud0503@gmail.com>
2016-03-22 20:50:03 +0900

    #1 Import project

---
+++ app/src/main/java/kr/hs/mirim/good005003/helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.mirim.good005003.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
